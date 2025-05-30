d8ae95f1ca897fa8d24219e0a1364ec454a24e2c
Giovanni Rodriguez <leandroide888@gmail.com>
2025-05-30 02:55:08 -0300

    Evitar reinicios y asegurar unica maquina ejecutando, nestasyncio eliminado, requeriments actualizado para no instalar nestasyncio

--- main.py
+++ main.py
@@ -4,6 +4,4 @@ import asyncio
 from telegram_bot import iniciar_bot
 
 if __name__ == "__main__":
-    import nest_asyncio
-    nest_asyncio.apply()  # <- Evita conflicto si ya hay un loop
-    asyncio.run(iniciar_bot())
+    asyncio.run(iniciar_bot()) 
